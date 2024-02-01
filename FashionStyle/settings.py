e3b4014103db600f7210c2bb8cc4a6ce2e4e5419
FarhanAmir306 <amirf3051@gmail.com>
2024-02-01 18:58:46 +0600

    last

--- FashionStyle/settings.py
+++ FashionStyle/settings.py
@@ -27,10 +27,9 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-
 ALLOWED_HOSTS = ['*']
 CORS_ORIGIN_ALLOW_ALL = True
-# CSRF_TRUSTED_ORIGINS = [] render link hobe 
+CSRF_TRUSTED_ORIGINS = ['https://fashionstyle-qmo0.onrender.com']
 
 
 
