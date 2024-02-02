c7b65611c41d1a7ed63ed87f268b7da2f7f2d8f9
FarhanAmir306 <amirf3051@gmail.com>
2024-02-03 00:13:02 +0600

    database change

--- FashionStyle/settings.py
+++ FashionStyle/settings.py
@@ -84,20 +84,20 @@ WSGI_APPLICATION = 'FashionStyle.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-    'default': dj_database_url.config(
-        # Feel free to alter this value to suit your needs.
-        default='postgres://fashionstyle_user:Ec4LBG2HuEl2WphNVl3gD9KTgk874xg7@dpg-cmtorngl5elc73ejimg0-a.oregon-postgres.render.com/fashionstyle',
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         # Feel free to alter this value to suit your needs.
+#         default='postgres://fashionstyle_user:Ec4LBG2HuEl2WphNVl3gD9KTgk874xg7@dpg-cmtorngl5elc73ejimg0-a.oregon-postgres.render.com/fashionstyle',
+#         conn_max_age=600
+#     )
+# }
 
 
 # Password validation
@@ -136,8 +136,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    
+]
 MEDIA_URL = '/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
